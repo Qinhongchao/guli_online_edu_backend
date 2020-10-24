4bf6f7e1adee6cf6e7ccf9cc3619acf8fc790088
scruic <Hongchao_Qin@saic-gm.com>
2020-10-24 15:22:23 +0800

    init project

---
+++ service/service_edu/src/main/java/com/atguigu/eduservice/mapper/EduTeacherMapper.java
@@ -0,0 +1,16 @@
+package com.atguigu.eduservice.mapper;
+
+import com.atguigu.eduservice.entity.EduTeacher;
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+
+/**
+ * <p>
+ * 讲师 Mapper 接口
+ * </p>
+ *
+ * @author starshipz
+ * @since 2020-10-24
+ */
+public interface EduTeacherMapper extends BaseMapper<EduTeacher> {
+
+}
